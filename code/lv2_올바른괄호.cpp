44e75acf0242eef72420de5d7bce25b95c076f2a
GyeongahNa <gyeongahna@naver.com>
2022-08-16 16:34:20 +0900

    [lv2]올바른괄호 추가

---
+++ code/lv2_올바른괄호.cpp
@@ -0,0 +1,69 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <tuple>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cctype>
+#include <iostream>
+
+using namespace std;
+using ii = pair<int, int>;
+using iii = tuple<int, int, int>;
+
+#define X first
+#define Y second
+
+bool solution(string s) {
+
+    stack<char> st;
+
+    for (auto x : s) {
+        if (x == '(') st.push(x);
+        else if (st.empty()) return false;
+        else st.pop();
+    }
+    return st.empty();
+}
