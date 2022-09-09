e23dcdbaa755a3b6e10e2328545f50157d758e68
GyeongahNa <gyeongahna@naver.com>
2022-09-09 11:19:35 +0900

    [lv3]기지국설치 추가

---
+++ code/lv3_기지국설치.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <tuple>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+using ii = pair<int, int>;
+using iii = tuple<int, int, int>;
+
+#define X first
+#define Y second
+
+int solution(int n, vector<int> stations, int w) {
+
+    vector<ii> v;
+
+    for (auto x : stations) {
+        int st = max(0, x-w);
+        int en = min(n, x+w);
+        int sz = (int)v.size();
+        if (!sz || st > v[sz-1].Y+1) v.push_back({st, en});
+        else v[sz-1].Y = max(v[sz-1].Y, en);
+    }
+
+    int p = 1, cnt = 0;
+    for (auto [st, en] : v) {
+        cnt += (st-p+2*w)/(2*w+1);
+        p = en+1;
+    }
+
+    if (p >= n+1) return cnt;
+    return cnt += (n+1-p+2*w)/(2*w+1);
+}
